b53e4945df0f6298a01af250b8d685f83ed62cda
Maha Shahzadi <104192800+MahaAbdulMukhtar@users.noreply.github.com>
2026-08-14 10:04:28 +0000

    RESTFUL API

---
+++ project-01/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const users = require('./MOCK_DATA.json');
+
+const app = express();
+const PORT = 8000;
+
+// Routes
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+        ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    return res.send(html);
+});
+
+// REST API
+app.get("/api/users", (req, res) => {
+    return res.json(users);
+});
+
+app.route("/api/users/:id")
+.get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
+})
+.patch((req, res) => {
+    //TODO: Edit the user with id
+    return res.json({status: "Pending"});
+})
+.delete((req, res) => {
+    //TODO: Delete the user with id
+    return res.json({status: "Pending"});
+});
+
+app.post("/api/users", (req, res) => {
+    //TODO: Create new user
+    res.json({status: "pending"})
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
