d7c93c89debd57261b1d2e616211b30f6211ffbf
MahaAbdulMukhtar <mahaabdulmukhtar@gmail.com>
2026-08-18 12:55:20 +0500

    Cookies

--- short-url/middlewares/auth.js
+++ short-url/middlewares/auth.js
@@ -1,9 +1,10 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"];
 
   if (!userUid) return res.redirect("/login");
+  const token = userid.split("Bearer ")[1];
   const user = getUser(userUid);
 
   if (!user) return res.redirect("/login");
@@ -13,9 +14,10 @@ async function restrictToLoggedinUserOnly(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"];
+  const token = userid.split("Bearer ")[1];
 
-  const user = getUser(userUid);
+  const user = getUser(token);
 
   req.user = user;
   next();
