6e79a82aceaa49199a844803c2ab7a1ed34c156c
MahaAbdulMukhtar <mahaabdulmukhtar@gmail.com>
2026-08-18 11:53:59 +0500

    URL Shortner

---
+++ short-url/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require("./connection");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+
+const URL = require("./models/url");
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+
+const app = express();
+const PORT = 8001;
+
+// Connection
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("MongoDB Connected!"),
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortID = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortID,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    },
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
